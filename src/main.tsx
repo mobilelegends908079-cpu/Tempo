e0fb41b010d281acdaa3f81adb78755f3cb9971d
mobilelegends908079-cpu <mobilelegends908079@gmail.com>
2026-09-14 17:00:50 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
